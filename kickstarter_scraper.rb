a699bc9558ee7b8ae86ddef2422312c81a6e1c87
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-20 22:21:15 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,7 @@ def create_project_hash
   # projects: kickstarter.css("li.project.grid_4")
   # name: project.css("h2.bbcard_name strong a").text 
   # img link: project.css("div.project-thumbnail a img").attribute("src").value
-  # description: project.css("p.bbcard_burb").text
+  # description: project.css("p.bbcard_blurb").text
   
   binding.pry
 end
