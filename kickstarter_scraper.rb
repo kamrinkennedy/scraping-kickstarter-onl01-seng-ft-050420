cd43f24e684d36ce84b203c3ecb56934156cf68d
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-20 22:15:17 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,8 +8,9 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   
   # projects: kickstarter.css("li.project.grid_4")
-  # names: project.css("h2.bbcard_name strong a").text 
+  # name: project.css("h2.bbcard_name strong a").text 
   # img link: project.css("div.project-thumbnail a img").attribute("src").value
+  # description: project.css("p.bbcard_burb").text
   binding.pry
 end
 
